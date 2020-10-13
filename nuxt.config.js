b8c53af0fdad14f341824d10039b80fddab10139
Koichi Hirachi <kouichi.hirachi@gmail.com>
2020-10-14 00:14:48 +0900

    Type Scriptに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxt/typescript-build'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
